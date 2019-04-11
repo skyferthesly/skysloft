8fe399db13a96e581236fcab8c6cc310c4f67432
Skyler Moore-Firkins <skyler.moorefirkins@skysloft.com>
2019-04-11 02:35:33 -0400

    removed build/ from .gitignore

---
+++ build/precache-manifest.f34be46ddd4a0bc88071d98b3eb63f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "279f4bee31345071e58a",
+    "url": "/static/js/main.929d533a.chunk.js"
+  },
+  {
+    "revision": "b08e6d47a46faa2a9e47",
+    "url": "/static/js/2.f29d39db.chunk.js"
+  },
+  {
+    "revision": "279f4bee31345071e58a",
+    "url": "/static/css/main.5f359301.chunk.css"
+  },
+  {
+    "revision": "e3036c7c608e9ef5cb3092cc7c4122b3",
+    "url": "/index.html"
+  }
+];
